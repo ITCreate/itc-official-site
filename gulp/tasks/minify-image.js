60caeb830ca20488a109c855ed07f6b1b1f7506c
Takuya Fukuju <chalkygames123@icloud.com>
2016-11-14 23:54:28 +0900

    minify-image タスクで大きすぎる画像のリサイズも行うよう変更。
    * プロフィール画像を正方形に切り抜くよう変更。
    * ImageMagick に依存するようになった点に注意。
    * PNG 画像の圧縮率をより高く変更。

--- gulp/tasks/minify-image.js
+++ gulp/tasks/minify-image.js
@@ -6,23 +6,41 @@ var serve = require('./serve');
 var browserSync = require('browser-sync').get('Server 1');
 
 var minifyImage = function() {
-	gulp.src(config.src + 'images/**/*.{gif,jpg,png,svg}')
-		.pipe($.imagemin([
-				$.imagemin.gifsicle({
-					optimizationLevel: 3,
-				}),
-				imageminPngquant({
-					quality: '80-90',
-					speed: 1,
-				}),
-				$.imagemin.jpegtran({
-					progressive: true,
-				}),
-				$.imagemin.svgo(),
-		]))
-		.pipe($.imagemin())
+	return gulp.src([config.src + 'images/**/*', '!' + config.src + 'images/**/profile-b*.{gif,jpg,png}'])
+		.pipe($.imageResize({
+			width: 1200,
+			imageMagick: true,
+		}))
 		.pipe(gulp.dest(config.dest + 'images/'))
-		.pipe(browserSync.stream({match: config.dest + 'images/**/*.{gif,jpg,png,svg}'}));
+		.on('end', function() {
+			gulp.src(config.src + 'images/**/profile-b*.{gif,jpg,png}')
+				.pipe($.imageResize({
+					width: 160,
+					height: 160,
+					crop: true,
+					imageMagick: true,
+				}))
+				.pipe(gulp.dest(config.dest + 'images/'))
+				.on('end', function() {
+					gulp.src(config.dest + 'images/**/*.{gif,jpg,png,svg}')
+					.pipe($.imagemin([
+						$.imagemin.gifsicle({
+							optimizationLevel: 3,
+						}),
+						imageminPngquant({
+							quality: '70-80',
+							speed: 1,
+						}),
+						$.imagemin.jpegtran({
+							progressive: true,
+						}),
+						$.imagemin.svgo(),
+					]))
+					.pipe($.imagemin())
+					.pipe(gulp.dest(config.dest + 'images/'))
+					.pipe(browserSync.stream({match: config.dest + 'images/**/*.{gif,jpg,png,svg}'}));
+				});
+		});
 };
 
 gulp.task('minify-image', minifyImage);

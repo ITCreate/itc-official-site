fa67b98db831d1581ca2e99567606c96a4008a26
Takuya Fukuju <chalkygames123@icloud.com>
2017-04-12 23:09:18 +0900

    部員のプロフィールを JSON で管理するよう変更。

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,6 +8,9 @@ var watch = function() {
 	$.watch(config.src + '/ejs/**/*.ejs', function() {
 		return gulp.start('html');
 	});
+	$.watch(config.src + '/json/profiles.json', function() {
+		return gulp.start('html');
+	});
 	$.watch(config.src + '/scss/**/*.scss', function() {
 		return gulp.start('css');
 	});

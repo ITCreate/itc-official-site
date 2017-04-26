8bb38d0de89d95a11201b80fd06d7a137c1697ac
Takuya Fukuju <chalkygames123@icloud.com>
2017-04-26 17:29:30 +0900

    node-sass: includePaths で node-font-awesome を読み込み。
    * copy タスクに Font Awesome フォントファイルのコピー処理を追加。

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -4,6 +4,10 @@ var config = require('../config');
 var copy = function() {
 	return gulp.src(config.src + '/fonts/**/*.woff2')
 		.pipe(gulp.dest(config.dest + '/assets/fonts'))
+		.on('end', function() {
+			gulp.src(require('node-font-awesome').fonts)
+				.pipe(gulp.dest(config.dest + '/assets/fonts'));
+		});
 };
 
 gulp.task('copy', copy);

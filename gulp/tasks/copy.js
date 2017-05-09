96b7fdff16725b7f2d72aa3e6d8c1657300938f4
Takuya Fukuju <chalkygames123@icloud.com>
2017-05-09 18:22:00 +0900

    ファビコンを追加。

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -4,10 +4,14 @@ var config = require('../config');
 var copy = function() {
 	return gulp.src(config.src + '/fonts/**/*.woff2')
 		.pipe(gulp.dest(config.dest + '/assets/fonts'))
+	.on('end', function() {
+		gulp.src(require('node-font-awesome').fonts)
+			.pipe(gulp.dest(config.dest + '/assets/fonts'))
 		.on('end', function() {
-			gulp.src(require('node-font-awesome').fonts)
-				.pipe(gulp.dest(config.dest + '/assets/fonts'));
+			gulp.src(config.src + '/favicon.ico')
+				.pipe(gulp.dest(config.dest));
 		});
+	});
 };
 
 gulp.task('copy', copy);
